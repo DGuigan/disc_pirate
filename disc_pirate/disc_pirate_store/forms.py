8c3b650e84abe6f4aa03a07ed9d2bb6c214ce6c9
herronk2 <kyle.herron2@mail.dcu.ie>
2021-02-24 17:45:21 +0000

    Added CASignupForm form

--- disc_pirate/disc_pirate_store/forms.py
+++ disc_pirate/disc_pirate_store/forms.py
@@ -1,8 +1,25 @@
 from django.forms import ModelForm
 from .models import Album
+from .models import CaUser
+from django.contrib.auth.forms import UserCreationForm
+from django.db import transaction
 
 
 class AlbumForm(ModelForm):
     class Meta:
         model = Album
         fields = ['albumName', 'artist', 'genre','releaseDate', 'description', 'price']
+
+
+class CASignupForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = CaUser
+
+    @transaction.atomic
+    def save(self):
+        #  create object in memory, but don't add to db
+        user = super().save(commit=False)
+
+        user.is_admin = False
+        user.save()
+        return user
