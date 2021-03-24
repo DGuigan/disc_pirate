827603cbd9e96750de403ea81caa3e0103e6b68c
David McGuigan <david.mcguigan2@mail.dcu.ie>
2021-03-24 18:26:09 +0000

    Added blank user page, moved log out button into it

--- disc_pirate/disc_pirate_store/views.py
+++ disc_pirate/disc_pirate_store/views.py
@@ -181,6 +181,11 @@ def admin_page(request):
         return render(request, 'admin_page.html', {'orders': orders, 'form': form})
 
 
+@login_required
+def user_page(request):
+    return render(request, 'user_page.html')
+
+
 class UserViewSet(viewsets.ModelViewSet):
     queryset = CaUser.objects.all()
     serializer_class = UserSerializer
