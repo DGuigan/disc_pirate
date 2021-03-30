2e2199accc74ddcb3d3b3b4ecadbb11725214b4c
David McGuigan <david.mcguigan2@mail.dcu.ie>
2021-03-30 13:23:11 +0100

    node and django are now best friends

--- disc_pirate/disc_pirate/settings.py
+++ disc_pirate/disc_pirate/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'disc_pirate_store'
 ]
 
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'disc_pirate.urls'
@@ -137,3 +139,8 @@ REST_FRAMEWORK = {
     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
  ]
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
