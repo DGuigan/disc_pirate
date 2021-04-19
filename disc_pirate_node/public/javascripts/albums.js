d07d8094af8a85a18b89d94c9e1b8dbcbe4c79bc
David McGuigan <david.mcguigan2@mail.dcu.ie>
2021-04-19 14:44:29 +0100

    view basket in node

--- disc_pirate_node/public/javascripts/albums.js
+++ disc_pirate_node/public/javascripts/albums.js
@@ -1,4 +1,4 @@
-function populateTable() {
+function showAlbums() {
     fetch("http://localhost:8000/api/albums/?format=json")
       .then(response => response.json())
       .then(data => {
@@ -35,7 +35,35 @@ function populateTable() {
           artTd.appendChild(art);
         })
       })
+    
+    // reverse button function
+    let album_button = document.getElementById("album-button");
+    album_button.onclick = hideAlbums;
+    album_button.innerHTML = "Hide Albums";
     console.log("Albums loaded");
+}
+
+function hideAlbums() {
+
+  // create blank table
+  let table = document.getElementById("album-table");
+  table.innerHTML = "";
+  let newRow = document.createElement("tr");
+  table.appendChild(newRow);
+
+  let headings = ["", "Album", "Artist", "Genre", "Cover"];
+
+  headings.forEach(heading => {
+    let newCol = document.createElement("th");
+    newRow.appendChild(newCol);
+    newCol.innerHTML = heading;
+  })
+
+  // reverse button function
+  let album_button = document.getElementById("album-button");
+    album_button.onclick = showAlbums;
+    album_button.innerHTML = "Show Albums";
+    console.log("Albums hidden");
 }
 
 function addToBasket(albumId) {
@@ -55,4 +83,65 @@ function addToBasket(albumId) {
   }
 }
 
-populateTable();
+function showBasket() {
+  if (window.token) {
+    fetch("http://localhost:8000/view_basket?format=json", {
+      method: 'GET',
+      headers :{
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': `Token ${window.token}`
+      }
+    }).then(response => response.json())
+      .then(data => {
+        let table = document.getElementById("basket-table");
+        data.forEach(element => {
+          let newRow = document.createElement("tr");
+          table.appendChild(newRow);
+
+          let name = document.createElement("td");
+          let artist = document.createElement("td");
+          let quantity = document.createElement("td");
+          
+          name.innerHTML = element['albumName'];
+          artist.innerHTML = element['artist'];
+          quantity.innerHTML = element['quantity'];
+
+          newRow.appendChild(name);
+          newRow.appendChild(artist);
+          newRow.appendChild(quantity);
+        })
+      })
+
+    let album_button = document.getElementById("basket-button");
+    album_button.onclick = hideBasket;
+    album_button.innerHTML = "Hide Basket";
+    console.log("Albums loaded");
+    }
+    else {
+      alert("Sign in");
+    }
+}
+
+function hideBasket() {
+
+  // create blank table
+  let table = document.getElementById("basket-table");
+  table.innerHTML = "";
+  let newRow = document.createElement("tr");
+  table.appendChild(newRow);
+
+  let headings = ["Album", "Artist", "Quantity"];
+
+  headings.forEach(heading => {
+    let newCol = document.createElement("th");
+    newRow.appendChild(newCol);
+    newCol.innerHTML = heading;
+  })
+
+  // reverse button function
+  let album_button = document.getElementById("basket-button");
+    album_button.onclick = showBasket;
+    album_button.innerHTML = "Show Basket";
+    console.log("Basket hidden");
+}
