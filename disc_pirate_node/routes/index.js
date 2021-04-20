74ea201fa123d4b6b7372c0835f28f0b4b8b4ce3
David McGuigan <david.mcguigan2@mail.dcu.ie>
2021-04-20 14:36:14 +0100

    I have learned to route a page I think

--- disc_pirate_node/routes/index.js
+++ disc_pirate_node/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/albums', function (req, res) {
+  res.render('albums', { title: 'All Albums'});
+});
+
 module.exports = router;
