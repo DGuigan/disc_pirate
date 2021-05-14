2a9e0905c0dbfa771ce293ef31a2ac68ea0796c0
David McGuigan <david.mcguigan2@mail.dcu.ie>
2021-05-14 15:07:54 +0100

    fixed spelling error in title

--- disc_pirate_node/routes/index.js
+++ disc_pirate_node/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Disk Pirate' });
+  res.render('index', { title: 'Disc Pirate' });
 });
 
 module.exports = router;
